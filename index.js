3bbc8d840fa51d9224d2099bb770127108f051ef
Mekesia Brown <mekesiabrown@gmail.com>
2023-04-16 04:16:02 -0400

    refactor: general

--- index.js
+++ index.js
@@ -1,43 +1,20 @@
 const express = require('express');
-const cheerio = require('cheerio');
-const axios = require('axios');
 
-const { handle_response, get_all_news } = require('./lib/sources');
+const { handle_one_source_response, news_items } = require('./lib/sources');
 const app = express();
-
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-    }
-);
-
-app.get('/news', (req, res) => {
-    const news_items = get_all_news();
-    
-    res.json({news_items});
+    res.status(200).json(news_items);
 });
 
-app.get('/news/:news_item_id', async (req, res) => {
+app.get('/:news_item_id', async (req, res) => {
     const news_item_id = req.params.news_item_id;
+    const news_url = news_items.filter(newspaper => newspaper[0].source === news_item_id);
 
-    // const news_item_url = news_items.filter(source => source.source === news_item_id)[0].url;
-
-    // if (!news_item_url) {
-    //     res.status(404).send('News source not found');
-    // }
-
-    // res.status(200).send(news_item_url);
-
-    // axios.get(news_source.url)
-    //     .then(response => {
-    //         const ans = handle_response(response, news_source);
-
-    //         res.json({ans});
-    //     });
+    res.status(200).json({ content: news_url });
 });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-    }
-);
+});
