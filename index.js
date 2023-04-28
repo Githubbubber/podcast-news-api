8cac2d408ec7531036f25f81a3b435425a05c8d8
Mekesia Brown <mekesiabrown@gmail.com>
2023-04-28 10:51:25 -0400

    refactor: update import variable name; temp json

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const express = require('express');
 
-const { handle_one_source_response, news_items } = require('./lib/sources');
+const { get_all_news_items } = require('./lib/sources');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.status(200).json(news_items);
+    res.status(200).json({see: "list of news items"});
 });
 
 app.get('/:news_item_id', async (req, res) => {
-    const news_item_id = req.params.news_item_id;
-    const news_url = news_items.filter(newspaper => newspaper[0].source === news_item_id);
-
-    res.status(200).json({ content: news_url });
+    res.status(200).json({ content: "news_url" });
 });
 
 app.listen(port, () => {
